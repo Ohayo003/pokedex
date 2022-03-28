3fcf791265d0e3c2602a5b41982463b71b58aa8b
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-28 22:46:41 +0800

    added fallback image if pokemon does not exist

---
+++ src/components/widgets/FallBackImage.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import {
+  Image as CustomImage,
+  ImageProps as CustomImageProps,
+} from "@chakra-ui/react";
+
+interface ImageProps {
+  src: string;
+  /**
+   *
+   * Displayed while the image is still loading
+   *
+   */
+  loader?: JSX.Element | string;
+  /**
+   *
+   * The fallback image url.
+   * Image will result to error if fallback url does not exist
+   *
+   */
+  fallback?: string;
+}
+
+const previouslyLoadedImage: string[] = [];
+
+const FallBackImage = ({
+  src,
+  loader,
+  fallback,
+  ...props
+}: ImageProps & Omit<CustomImageProps, "fallback">) => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (previouslyLoadedImage.includes(src)) return setLoading(false);
+
+    const image = new Image();
+    image.src = src;
+    image.onload = () => {
+      previouslyLoadedImage.push(src);
+      setLoading(false);
+    };
+
+    image.onerror = () => {
+      setError(true);
+      setLoading(false);
+    };
+
+    return () => {
+      setLoading(true);
+      setError(false);
+
+      image.onload = null;
+      image.onerror = null;
+    };
+  }, [src]);
+
+  return (
+    <>
+      {!!loading && loader}
+      {!loading && (
+        <CustomImage src={!error ? src : fallback} loading="lazy" {...props} />
+      )}
+    </>
+  );
+};
+
+export default FallBackImage;
