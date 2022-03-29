bff476e5a8abd8aae969cf8a53c78249c17cf255
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-29 18:36:46 +0800

    change the current fallback image to next/image

--- src/components/widgets/Pokemon/PokemonGridView.tsx
+++ src/components/widgets/Pokemon/PokemonGridView.tsx
@@ -71,7 +71,7 @@ const PokemonGridView = ({ pokemons }: PokemonGridViewType) => {
                   borderWidth="2px 2px 6px 2px"
                   position="relative"
                 >
-                  <FallBackImage
+                  {/* <FallBackImage
                     src={pokemonImage(pokemon?.id!)}
                     loader={
                       <Box>
@@ -79,8 +79,8 @@ const PokemonGridView = ({ pokemons }: PokemonGridViewType) => {
                       </Box>
                     }
                     fallback="/assets/background/whos_that_pokemon.jpg"
-                  />
-                  {/* <Image
+                  /> */}
+                  <Image
                     src={pokemonImage(pokemon?.id!)}
                     alt={`${pokemon.name}`}
                     layout="fill"
@@ -88,7 +88,7 @@ const PokemonGridView = ({ pokemons }: PokemonGridViewType) => {
                     // height={20}
                     // objectFit="cover"
                     // objectPosition="center"
-                  /> */}
+                  />
                 </Box>
                 <Box
                   pt={{ base: 3, lg: 1 }}
