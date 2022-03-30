737f2c05e06839901aa2888b5fc52e7bef39fbd7
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-30 10:13:48 +0800

    added setLoading state

--- src/components/widgets/Pokemon/Pagination.tsx
+++ src/components/widgets/Pokemon/Pagination.tsx
@@ -1,6 +1,6 @@
 import { Button, Flex, HStack, Icon } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { Dispatch } from "react";
 import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 import useStore from "src/hooks/useStore";
 
@@ -9,6 +9,7 @@ interface IPagination {
   nextPage: () => void;
   selectedPage: (idx: number) => void;
   numberOfPages: number[];
+  setLoading: Dispatch<React.SetStateAction<boolean>>;
   currentData: () => any[];
 }
 
@@ -16,17 +17,28 @@ const Pagination = ({
   previousPage,
   nextPage,
   numberOfPages,
+  setLoading,
   selectedPage,
   currentData,
 }: IPagination) => {
   const router = useRouter();
-  const currentPage = useStore(state=>state.currentPage)
+  const currentPage = useStore((state) => state.currentPage);
   const currentIndex = useStore((state) => state.currentIndex);
   const currentLastIndex = useStore((state) => state.currentLastIndex);
+
+  const handleNextPage = () => {
+    previousPage();
+    setLoading(true);
+  };
+  const handlePrevPage = () => {
+    nextPage();
+    setLoading(true);
+  };
+
   return (
     <Flex justifyContent="center" mt={2} gap={6} align="center" zIndex={1}>
       <Icon
-        onClick={previousPage}
+        onClick={handleNextPage}
         as={BiChevronLeft}
         w={{ lg: 6, base: 10 }}
         h={{ lg: 6, base: 10 }}
@@ -60,7 +72,7 @@ const Pagination = ({
         })}
       </HStack>
       <Icon
-        onClick={nextPage}
+        onClick={handlePrevPage}
         as={BiChevronRight}
         w={{ lg: 6, base: 10 }}
         h={{ lg: 6, base: 10 }}
