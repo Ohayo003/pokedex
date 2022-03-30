5fbc917a1a8dcb727f6353767ad67deecb566823
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-30 10:11:53 +0800

    added text element in Loading component

--- src/components/widgets/Loading.tsx
+++ src/components/widgets/Loading.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { VStack, Spinner } from "@chakra-ui/react";
+import { VStack, Spinner, Text } from "@chakra-ui/react";
 
-const Loading = () => {
+interface ILoading {
+  loadingText?: string;
+}
+
+const Loading = ({ loadingText }: ILoading) => {
   return (
     <VStack height="100vh" align="center" justify="center">
       <Spinner size="xl" thickness="6px" color="green" speed="0.65s" />
+      <Text
+        fontWeight="bold"
+        fontStyle="italic"
+        fontFamily="Inter"
+        fontSize="lg"
+        letterSpacing="wider"
+        color="text.light"
+      >
+        {loadingText}
+      </Text>
     </VStack>
   );
 };
